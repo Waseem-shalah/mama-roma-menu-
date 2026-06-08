84486f3324637faafe6f97eaf454401162862c86
Waseem shalah <waseemshalah@gmail.com>
2026-06-08 13:13:02 +0300

    update menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Divider from "../components/restaurant/Divider";
 import Footer from "../components/restaurant/Footer";
 import { menuData, languages } from "../components/restaurant/MenuData";
 
-const STARTERS_IMG = "../img1.jpg";
+const STARTERS_IMG = "../images/img1.jpg";
 const PASTA_IMG =
   "https://media.base44.com/images/public/69d0d56b9131865d8718912c/11f41452e_generated_4cd6dd4f.png";
 const WAGYU_IMG =
