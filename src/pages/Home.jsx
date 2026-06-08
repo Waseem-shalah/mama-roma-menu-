fe2de16f3674e94415f4aefec40a1835cbf9b0c9
Waseem shalah <waseemshalah@gmail.com>
2026-06-08 13:18:01 +0300

    fix starters local image import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,9 @@ import Divider from "../components/restaurant/Divider";
 import Footer from "../components/restaurant/Footer";
 import { menuData, languages } from "../components/restaurant/MenuData";
 
-const STARTERS_IMG = "../images/img1.jpg";
+// Import local image correctly
+import STARTERS_IMG from "../images/img1.jpg";
+
 const PASTA_IMG =
   "https://media.base44.com/images/public/69d0d56b9131865d8718912c/11f41452e_generated_4cd6dd4f.png";
 const WAGYU_IMG =
@@ -20,6 +22,7 @@ export default function Home() {
   useEffect(() => {
     const dir = languages.find((l) => l.code === lang)?.dir || "ltr";
     document.documentElement.setAttribute("dir", dir);
+
     return () => document.documentElement.setAttribute("dir", "ltr");
   }, [lang]);
 
@@ -34,16 +37,22 @@ export default function Home() {
         imageAlt="Appetizers"
         lang={lang}
       />
+
       <Divider />
+
       <MenuSection section={menuData.salads} reverse lang={lang} />
+
       <Divider />
+
       <MenuSection
         section={menuData.intermediate}
         image={RISOTTO_IMG}
         imageAlt="Risotto and gnocchi"
         lang={lang}
       />
+
       <Divider />
+
       <MenuSection
         section={menuData.pasta}
         image={PASTA_IMG}
@@ -52,14 +61,18 @@ export default function Home() {
         lang={lang}
         splitAt={9}
       />
+
       <Divider />
+
       <MenuSection
         section={menuData.meats}
         image={WAGYU_IMG}
         imageAlt="Special meats"
         lang={lang}
       />
+
       <Divider />
+
       <MenuSection section={menuData.desserts} lang={lang} />
 
       <Footer lang={lang} />
