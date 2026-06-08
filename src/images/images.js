22e438c9780837f5e2a2dd997a89883bc881f6ff
Waseem shalah <waseemshalah@gmail.com>
2026-06-08 12:49:39 +0300

    normalize image imports to lowercase

--- src/images/images.js
+++ src/images/images.js
@@ -1,15 +1,15 @@
-import pomi from "./pomi.JPG";
+import pomi from "./pomi.jpg";
 import focaccia from "./Focaccia.jpg";
 import carpaccio from "./carpacio.jpeg";
 import vitelloTonato from "./vitelloTonato.jpg";
 import tartar from "./tartar.jpeg";
-import salmonTabioka from "./tabiokaSalmon.JPG";
-import arnchine from "./arnchine.PNG";
+import salmonTabioka from "./tabiokaSalmon.jpg";
+import arnchine from "./arnchine.png";
 import assadoKroketa from "./assadoKroketa.jpeg";
 import shrimps from "./shrimps.jpg";
 
 import cesar from "./cesar.jpg";
-import tabouleh from "./tabouleh.PNG";
+import tabouleh from "./tabouleh.png";
 import totani from "./totani.jpg";
 import cavolo from "./cavolo.jpg";
 
@@ -20,21 +20,21 @@ import scampi from "./scampi.jpeg";
 import salmonRissoto from "./salmonRissoto.jpg";
 import zaafaran from "./zaafaran.jpeg";
 import gnocciVerde from "./gnocciVerde.jpg";
-import rissotoAlloScoglio from "./rissotoAlloScoglio.PNG";
+import rissotoAlloScoglio from "./rissotoAlloScoglio.png";
 import rissotoAnimelle from "./rissotoAnimelle.jpeg";
 
 import pastaCreamTomato from "./pastaCreamTomato.jpeg";
-import carbonara from "./carbonara.JPG";
+import carbonara from "./carbonara.jpg";
 import funghi from "./funghi.jpeg";
 import pakriAussubuko from "./pakriAussubuko.jpeg";
-import pastaNera from "./pastaNera.PNG";
-import polenta from "./polenta.JPG";
+import pastaNera from "./pastaNera.png";
+import polenta from "./polenta.jpg";
 import raviolliParmisan from "./raviolliParmisan.jpg";
-import lasagna from "./lasagnia.JPG";
+import lasagna from "./lasagnia.jpg";
 import macroni from "./macroni.jpeg";
 
 import pizzacarbonara from "./pizzacarbonara.jpeg";
-import pizzaporcini from "./pizzaporcini.JPG";
+import pizzaporcini from "./pizzaporcini.jpg";
 import pizzaPepproni from "./pizzapapproni.jpeg";
 import pizzaMargarita from "./pizzamargarita.jpeg";
 import pizzaOssubuko from "./pizzaosoboco.jpeg";
@@ -43,11 +43,11 @@ import pizzaSeafood from "./pizzaseafood.jpg";
 
 import fillet from "./senta.jpg";
 import ribs from "./ribs.jpeg";
-import sienta from "./sienta.JPG";
+import sienta from "./sienta.jpg";
 
 import tiramisu from "./tiramisu.jpg";
-import cheesecake from "./cheesecake.PNG";
-import moose from "./moose.PNG";
+import cheesecake from "./cheesecake.png";
+import moose from "./moose.png";
 import creme from "./creme.jpg";
 import panacota from "./panacota.jpg";
 
